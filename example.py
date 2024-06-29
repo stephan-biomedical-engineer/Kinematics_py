1e91120a02a509f99fadf941a76dc3a172da6a61
stephan <stephanbrrs8@gmail.com>
2024-06-29 11:37:47 -0300

    Coments

--- example.py
+++ example.py
@@ -3,14 +3,17 @@ from dh import Joint, ForwardKinematicsDH
 from inversekinematics import InverseKinematics
 
 def main():
+    # Definição das juntas do manipulador, com os parâmetros de Denavit-Hartenberg
     Base = Joint(0.5, 0.2, 0, -90)
     Shoulder = Joint(0, 0.5, 45, 0)
     Elbow = Joint(0, 0.3, 0, -90)
     Wrist1 = Joint(0, 0.4, 90, 90)
     Wrist2 = Joint(0, 0, 0, -90)
 
+    # Cálculo da cinemática direta
     fk = ForwardKinematicsDH(Base, Shoulder, Elbow, Wrist1, Wrist2)
     
+    # Cálculo da cinemática inversa
     ik = InverseKinematics(fk)
     desired_position = np.array([0.3, 0.4, 0.5])
     joint_angles = ik.compute_ik(desired_position)
